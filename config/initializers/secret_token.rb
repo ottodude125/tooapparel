f0f3c5660da0095c33fa9bf7f33002eac873df32
Jonathan Katon <jonathan.katon@teradyne.com>
2012-12-09 17:33:57 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tooapparel::Application.config.secret_token = 'fdb5b59566c70d6b4e3bdec7825a2b0652731c76e42e7565f2792e2bd8290e988b400003892816895e5e483c7d1ce62b3fd22ebf21ad93cbbe51c0faae854648'
